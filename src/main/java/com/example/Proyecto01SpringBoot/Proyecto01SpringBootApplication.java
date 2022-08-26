9b9eba96a5f235f9ee352e0a09a1ec9e6178d435
Mateo8a <mateo.rodriguez@uptc.edu.co>
2022-08-25 20:06:16 -0500

    Primer proyecto springboot

---
+++ src/main/java/com/example/Proyecto01SpringBoot/Proyecto01SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.Proyecto01SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto01SpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Proyecto01SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto01SpringBoot/Proyecto01SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto01SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto01SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
